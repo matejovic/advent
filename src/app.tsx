e042f9124b884312021c8cae44859cce4403cf9f
Matej Vrzala <vrzala.matej@gmail.com>
2023-11-15 13:32:09 +0100

    a bit of interactivity

--- src/app.tsx
+++ src/app.tsx
@@ -1,31 +1,99 @@
-import { useState } from 'preact/hooks'
-import preactLogo from './assets/preact.svg'
-import viteLogo from '/vite.svg'
-import './app.css'
+import { useState } from "preact/hooks";
+import preactLogo from "./assets/preact.svg";
+import viteLogo from "/vite.svg";
+import "./app.css";
 
 export function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
+
+  const today = new Date();
+  const month = today.getMonth() + 1;
+  const year = today.getFullYear();
+  const day = today.getDate();
+
+  const [selectedDay, setSelectedDay] = useState(0);
+
+  // month is zero-indexed, and day 0 will return last day of previous month
+  function daysInMonth(month, year) {
+    return new Date(year, month, 0).getDate();
+  }
+
+  function getMonthName(monthNumber) {
+    const date = new Date();
+    date.setMonth(monthNumber - 1);
+
+    return date.toLocaleString("en-US", {
+      month: "long",
+    });
+  }
+
+  const daysInMonthArray = Array.from(
+    { length: daysInMonth(month, year) },
+    (_, index) => index + 1,
+  );
+
+  function openDayView(day, month, year) {
+    setSelectedDay(day);
+    //alert("OPEN DAY VIEW: " + day)
+  }
 
   return (
     <>
       <h2>Welcome to ProtoTypeZone</h2>
       <div className="calendarApp--screen-day-a">
-        <h3>Today is November, 7</h3>
+        <h3>
+          Today{" "}
+          {today.toLocaleDateString("default", {
+            weekday: "long",
+            year: "numeric",
+            month: "long",
+            day: "numeric",
+          })}
+        </h3>
       </div>
-      
-      <div className="calendarApp--screen-day-b">
-      
-      </div>
-      
-      <br/>
-      
+
+      <br />
+
       <div className="calendarApp--screen-month-a">
-        <h3>Welcome to November view a</h3>
-        
-        <div>
-          
+        <div class="xxx">
+          <h3>Welcome to {getMonthName(month)} View</h3>
+          <div>
+            <select class="month-select">
+              <option>Month</option>
+            </select>
+
+            <select class="year-select">
+              <option>Year</option>
+            </select>
+          </div>
+        </div>
+
+        <div class="day-in-month-wrap">
+          {daysInMonthArray.map((d) => (
+            <div
+              class="day-in-month"
+              onClick={() => openDayView(d, month, year)}
+            >
+              {d}
+            </div>
+          ))}
         </div>
       </div>
+
+      <br />
+
+      <div className="calendarApp--screen-day-b">
+        <h3>Plan and Review your days</h3>
+        {selectedDay ? (
+          <div>
+            {selectedDay}
+            <textarea className="day-planner" />
+            <button>Save</button>
+          </div>
+        ) : (
+          <p>no day was selected...</p>
+        )}
+      </div>
     </>
-  )
+  );
 }
